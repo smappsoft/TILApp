14e199c4d8dbeb907fe98ae7a1c9c863d5d95075
Mark Rubin <markr@smappsoft.com>
2018-07-29 07:13:09 -0700

    Add parent-chile relationship

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -25,6 +25,7 @@ struct AcronymsController: RouteCollection {
                                 acronym, updatedAcronym in
                                 acronym.short = updatedAcronym.short
                                 acronym.long = updatedAcronym.long
+                                acronym.userID = updatedAcronym.userID
                                 return acronym.save(on: req)
             }
         }
@@ -63,6 +64,13 @@ struct AcronymsController: RouteCollection {
                 .all()
         }
         
+        func getUserHandler(_ req: Request) throws -> Future<User> {
+            return try req.parameters.next(Acronym.self)
+                .flatMap(to: User.self)  { acronym in
+                    acronym.user.get(on: req)
+            }
+        }
+        
         acronymsRoutes.post(Acronym.self, use: createHandler)
         acronymsRoutes.get(Acronym.parameter, use: getHandler)
         acronymsRoutes.put(Acronym.parameter, use: updateHandler)
@@ -70,6 +78,7 @@ struct AcronymsController: RouteCollection {
         acronymsRoutes.get("search", use: searchHandler)
         acronymsRoutes.get("first", use: getFirstHandler)
         acronymsRoutes.get("sorted", use: sortedHandler)
+        acronymsRoutes.get(Acronym.parameter, "user", use: getUserHandler)
     }
     
     func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
