247b1625fe28b6c01aa071f0327c943a74c8fde0
Mark Rubin <markr@smappsoft.com>
2018-07-28 21:39:36 -0700

    Bugfix

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -19,9 +19,9 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     var databases = DatabasesConfig()
     let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
-    let username = Environment.get("DATABSE_USER") ?? "vapor"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
     let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-    let password = Environment.get("DATABASE_PASSWORD") ?? "PASSWORD"
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
     
     let databaseConfig = PostgreSQLDatabaseConfig(
         hostname: hostname,
